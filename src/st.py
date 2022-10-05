bb21e249cfffcdad18613a4e0c1e1f08a0b397d9
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-10-05 13:42:31 +0200

    Code restructured

--- src/st.py
+++ src/st.py
@@ -19,14 +19,17 @@ def getAllSuffixes(x):
         yield x[i:]+SENTINEL
     yield SENTINEL
 
-def getMatch(x: str, n: Node, c=SENTINEL):
+def getMatch(x: str, y: str, n: Node, i: int):
     nchild = n.child
     while type(nchild) is Node:
-        if x[nchild.start] == c:
-            return nchild
+        if x[nchild.start] == y[i]:
+            iters = 1
+            while nchild.start + iters < nchild.end and x[nchild.start+iters] == y[i+iters]:
+                iters += 1
+            return iters, nchild
         else:
             nchild = nchild.sib
-    return None
+    return -1, None
 
 def getDifference(str1, str2, i, j, length):
     counter = 0
@@ -49,28 +52,28 @@ def getSuffixTree(x: str):
         n = root
         i = 0
         while type(n) is Node:
-            match = getMatch(x, n, suf[i])
-            if match:
-                dif = getDifference(suf, x, i, match.start, match.end-match.start)
-                if dif<0:
-                    i += match.end-match.start
-                    n = match
-                    continue
-                else:
-                    newNode = Node(index+i+dif, len(x), index, n.child)
-                    middleNode = Node(match.start, match.start+dif, newNode, match.sib)
-                    n.child = middleNode
-                    
-                    match.start += dif+1
-                    match.sib = newNode
-                    painList.append(newNode)
-                    painList.append(middleNode)
-                    break
-            else:
+            dif, match = getMatch(x, suf, n, i)
+            if dif < 0:
+                # Finishes at the node
                 newNode = Node(index+i, len(x), index, n.child)
                 n.child = newNode
                 painList.append(newNode)
                 break
+            elif dif<match.end-match.start:
+                # Finishes at the edge
+                newNode = Node(index+i+dif, len(x), index, n.child)
+                middleNode = Node(match.start, match.start+dif, newNode, match.sib)
+                n.child = middleNode
+                
+                match.start += dif+1
+                match.sib = newNode
+                painList.append(newNode)
+                painList.append(middleNode)
+                break
+            else:
+                # We are still matching, search in match
+                i += match.end-match.start
+                n = match
         
     for p in painList:
         print(p)
